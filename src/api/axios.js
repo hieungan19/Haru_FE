8cc8e30b8617ce510660273851112514fadfbe12
Hieu Ngan <93175235+hieungan19@users.noreply.github.com>
2024-11-02 12:51:41 +0700

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-export default axios.create({
+const api = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
 });
+
+// Đặt header cho mọi yêu cầu
+api.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+
+export default api;
